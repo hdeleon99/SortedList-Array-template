7fbf67096ee10c76f9e0423840b08ef9afe1eec3
Isaiah Deleon <isaiahdeleon1999@gmail.com>
2021-09-22 19:20:29 -0700

    Fully implemented

--- SortedList.cpp
+++ SortedList.cpp
@@ -25,18 +25,54 @@ int SortedList<T>::GetLength() const {
 
 template<class T>
 bool SortedList<T>::Contains(T someItem) {
+    int midPoint;
+    int first = 0;
+    int last = length - 1;
 
+    while (first <= last){
+        midPoint = (first + last) / 2;
+
+        if (someItem < info[midPoint])
+            last = midPoint - 1;
+        else if (someItem > info[midPoint])
+            first = midPoint + 1;
+        else {
+            return true;
+        }
+    }
     return false;
 }
 
 template<class T>
 void SortedList<T>::PutItem(T item) {
+    int location = 0;
+    // find proper location for new element
+    for (location = 0; location < length; location++){
+        if (info[location] > item)
+            break;
+    }
 
+    // make room for new element in sorted list
+    for (int index = length; index > location; index--)
+        info[index] = info[index - 1];
+
+    info[location] = item;
+    length++;
 }
 
 template<class T>
 void SortedList<T>::DeleteItem(T item) {
-
+    int location;
+    // find item to delete
+    for (location = 0; location < length; location++){
+        if (info[location] == item)
+            break;
+    }
+    // remove space occupied by item, and shift elements accordingly
+    for (int i = location + 1; i < length; i++){
+        info[i-1] = info[i];
+    }
+    --length;
 }
 
 template<class T>
